c23ac8c1c0fb97470c22bc06e5385616733ed2a8
minalkhalid <160639621+minalkhalid@users.noreply.github.com>
2024-05-10 00:06:07 +0500

    Add files via upload

---
+++ todo_list.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let todoQuestions = await inquirer.prompt([
+        {
+            name: "fiestQuestion",
+            type: "input",
+            message: "what would you like to add in you todos?"
+        },
+        {
+            name: "secondQuestion",
+            type: "confirm",
+            message: "would you like to add more in your todos",
+            default: "true"
+        }
+    ]);
+    todos.push(todoQuestions.fiestQuestion);
+    console.log(todos);
+    condition = todoQuestions.secondQuestion;
+}
